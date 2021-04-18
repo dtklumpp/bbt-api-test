753ac1a6d5cff27e679a2a086310a2886d0500aa
dtklumpp <dtklumpp@gmail.com>
2021-04-18 14:16:35 -0400

    setup JWT tokens

--- server.js
+++ server.js
@@ -82,6 +82,52 @@ app.get('/addUser', (req, res) => {
     // res.render('api.ejs');
 })
 
+app.get('/addGame', (req, res) => {
+    console.log('top of game');
+
+    addGame();
+
+    async function addGame(){
+        try{
+            const bearerHeader = req.headers["authorization"];
+
+            console.log({bearerHeader});
+
+            if(typeof bearerHeader !== undefined){
+                const token = bearerHeader.split(" ")[1];
+                console.log({token});
+
+                const decodedToken = await auth.verifyIdToken(token);
+
+                const uid = decodedToken.uid
+
+                console.log({uid});
+            }
+
+
+            const docRef = await db.collection('dtk-test-3').add({
+                name: "Sample-Game",
+                day: "Thursday",
+                players: 4,
+            })
+            let docId = docRef.id;
+            console.log({docId});
+            res.status(201).json({ "doc": docId });
+        }
+        catch(err) {
+            console.error("Error adding game-doc", err);
+            res.send('failure '+err);
+        }
+        console.log('end fxn');
+    }
+
+
+// res.render('api.ejs');
+})
+
+
+
+
 app.get('/regUser', (req, res) => {
     console.log('top of reg');
 
